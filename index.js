353859e419b7bd4bc1a4c08ad14b81b6b937a39b
oppailibs <oppainekos@gmail.com>
2025-03-20 15:24:20 +0700

    ee_

--- index.js
+++ index.js
@@ -522,7 +522,7 @@ async function handleUDPOutbound(webSocket, responseHeader, log) {
     .pipeTo(
       new WritableStream({
         async write(chunk) {
-          const resp = await fetch("https://1.1.1.1/dns-query", {
+          const resp = await fetch("https://8.8.8.8/dns-query", {
             method: "POST",
             headers: {
               "content-type": "application/dns-message",
@@ -567,26 +567,3 @@ function safeCloseWebSocket(socket) {
   }
 }
 
-function generateUUIDv4() {
-  const randomValues = crypto.getRandomValues(new Uint8Array(16));
-  randomValues[6] = (randomValues[6] & 0x0f) | 0x40;
-  randomValues[8] = (randomValues[8] & 0x3f) | 0x80;
-  return [
-    randomValues[0].toString(16).padStart(2, '0'),
-    randomValues[1].toString(16).padStart(2, '0'),
-    randomValues[2].toString(16).padStart(2, '0'),
-    randomValues[3].toString(16).padStart(2, '0'),
-    randomValues[4].toString(16).padStart(2, '0'),
-    randomValues[5].toString(16).padStart(2, '0'),
-    randomValues[6].toString(16).padStart(2, '0'),
-    randomValues[7].toString(16).padStart(2, '0'),
-    randomValues[8].toString(16).padStart(2, '0'),
-    randomValues[9].toString(16).padStart(2, '0'),
-    randomValues[10].toString(16).padStart(2, '0'),
-    randomValues[11].toString(16).padStart(2, '0'),
-    randomValues[12].toString(16).padStart(2, '0'),
-    randomValues[13].toString(16).padStart(2, '0'),
-    randomValues[14].toString(16).padStart(2, '0'),
-    randomValues[15].toString(16).padStart(2, '0'),
-  ].join('').replace(/^(.{8})(.{4})(.{4})(.{4})(.{12})$/, '$1-$2-$3-$4-$5');
-}
